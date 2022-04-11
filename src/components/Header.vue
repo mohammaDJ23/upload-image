54c7fb21a39e7a065ceeb73c702c42344335e9fc
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2022-04-12 00:54:09 +0430

    building auth module

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,22 @@
 <template>
-  <div class="ui secondary pointing menu"><a href="/" class="active item">Image storage</a></div>
+  <div class="ui secondary pointing menu">
+    <a href="/" class="active item">Image storage</a>
+
+    <div class="right menu">
+      <a href="#" class="ui item" @click="login">Login</a>
+    </div>
+  </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+
 export default {
   name: 'Header',
+  methods: {
+    ...mapActions(['login']),
+
+    // define other methods here
+  },
 };
 </script>
